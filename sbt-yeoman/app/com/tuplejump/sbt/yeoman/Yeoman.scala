2d5278b4331ada31815dfc489fa0857e4124ff0d
Rohit Rai <rohit@tuplejump.com>
2013-05-11 12:31:43 +0530

    Adding licenses

--- sbt-yeoman/app/com/tuplejump/sbt/yeoman/Yeoman.scala
+++ sbt-yeoman/app/com/tuplejump/sbt/yeoman/Yeoman.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013 Tuplejump Software Pvt. Ltd.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 package com.tuplejump.sbt.yeoman
 
 import sbt._
@@ -6,13 +22,6 @@ import play.Project._
 import java.net.InetSocketAddress
 import scala.Some
 
-/**
- * Created with IntelliJ IDEA.
- * User: rohit
- * Date: 5/10/13
- * Time: 6:32 PM
- * To change this template use File | Settings | File Templates.
- */
 object Yeoman extends Plugin {
   val yeomanDirectory = SettingKey[File]("yeoman-directory")
 
