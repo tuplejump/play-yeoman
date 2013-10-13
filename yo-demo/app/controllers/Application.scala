c1959f298f5f5f6620910d0d1f9f13033e168266
Rohit Rai <rohit@tuplejump.com>
2013-10-13 10:28:05 +0530

    Updated sample to demonstrate normal play routes

--- yo-demo/app/controllers/Application.scala
+++ yo-demo/app/controllers/Application.scala
@@ -2,11 +2,17 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.json.Json
+import play.api.libs.json._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
-  
+
+  def json(keyword: String) = Action {
+    Ok(JsObject("keyword" -> JsString(keyword) :: Nil))
+  }
+
 }
