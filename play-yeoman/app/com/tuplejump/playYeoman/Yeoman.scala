2287e104c5c2926ba65595e2eeddf9349e124bb9
Rohit Rai <rohit@tuplejump.com>
2014-01-04 13:54:47 +0530

    Removed AsynResult use

--- play-yeoman/app/com/tuplejump/playYeoman/Yeoman.scala
+++ play-yeoman/app/com/tuplejump/playYeoman/Yeoman.scala
@@ -2,22 +2,26 @@ package com.tuplejump.playYeoman
 
 import play.api._
 import play.api.mvc._
+import controllers.{ExternalAssets, Assets}
 import play.api.Play.current
 import java.io.File
 import play.api.libs.MimeTypes
-import controllers.{ExternalAssets, Assets}
+import scala.concurrent.Future
+import play.api.libs.concurrent.Execution.Implicits._
 
 
 object Yeoman extends Controller {
 
-  def index = Action {
+  def index = Action.async {
     request =>
       if (request.path.endsWith("/")) {
-        AsyncResult {
-          at("index.html").apply(request)
-        }
+        //AsyncResult {
+        at("index.html").apply(request)
+        //}
       } else {
-        Redirect(request.path + "/")
+        Future {
+          Redirect(request.path + "/")
+        }
       }
   }
 
@@ -54,10 +58,16 @@ object DevAssets extends Controller {
    * absolute path is in the correct SBT sub-project.
    */
   def assetHandler(fileName: String): Action[AnyContent] = Action {
-    val targetPaths = basePaths.view map { new File(_, fileName) } // generate a non-strict (lazy) list of the full paths
+    val targetPaths = basePaths.view map {
+      new File(_, fileName)
+    } // generate a non-strict (lazy) list of the full paths
 
     // take the files that exist and generate the response that they would return
-    val responses = targetPaths filter { _.exists() } map { Ok.sendFile(_, inline = true).withHeaders(CACHE_CONTROL -> "no-store")}
+    val responses = targetPaths filter {
+      _.exists()
+    } map {
+      Ok.sendFile(_, inline = true).withHeaders(CACHE_CONTROL -> "no-store")
+    }
 
     // return the first valid path, return NotFound if no valid path exists
     responses.headOption getOrElse NotFound
