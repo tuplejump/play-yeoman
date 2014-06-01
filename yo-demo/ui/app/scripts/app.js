86bae0582d9af70c878971f735bb39365885cb3e
Shiti <ssaxena.ece@gmail.com>
2014-06-01 18:02:58 +0530

    fixed issue with stage

--- yo-demo/ui/app/scripts/app.js
+++ yo-demo/ui/app/scripts/app.js
@@ -1,26 +1,21 @@
 'use strict';
 
 angular.module('uiApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngRoute'
+  ]).config(function ($routeProvider) {
+    $routeProvider.when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
-      })
-	 .when('/myhome', {
-      templateUrl: 'views/myhome.html',
-      controller: 'MyhomeCtrl'
-     })
-     .when('/templ', {
-      templateUrl: 'views/templ.html',
-      controller: 'TemplCtrl'
-     })
-      .otherwise({
+      }).when('/myhome', {
+        templateUrl: 'views/myhome.html',
+        controller: 'MyhomeCtrl'
+      }).when('/templ', {
+        templateUrl: 'views/templ.html',
+        controller: 'TemplCtrl'
+      }).otherwise({
         redirectTo: '/'
       });
   });
